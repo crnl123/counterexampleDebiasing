97230727e9ddc013388fa3c621ee93e81e2fa1e9
crnl123 <lausundy@yahoo.com.hk>
2025-08-31 21:48:03 +0100

    plottings

--- plot_metrics.py
+++ plot_metrics.py
@@ -2,15 +2,34 @@ import pickle
 import numpy as np
 from matplotlib import pyplot as plt
 
-# File name here
-f = open('metrics/full_vs_75_metric.pickle', 'rb')
-metrics = pickle.load(f)
-f.close()
 
-# debias_quarters_P_match, watermark, debias_quarters, clean_mixed_marked_validation
+def calculate_quantiles(group):
+    return np.quantile(group, [.25, .75], axis=0)
+
+
+metrics = []
+
+filenames = [
+    'metrics/metrics_debias_naive_unbalanced_cleanmix.pickle',      # 0,1,2,3
+    'metrics/full_vs_75_metric.pickle',                             # 4,5,6,7,8,9
+    'metrics/quarters_vs_halves_metrics.pickle',                    # 10,11
+    'metrics/quarters_true_vs_false_metrics.pickle'                 # 12,13
+]
+
+for name in filenames:
+    f = open(name, 'rb')
+    metrics += pickle.load(f)
+    f.close()
+
+# # File name here
+# f = open('metrics/THIS_FILE_DOES_NOT_EXIST', 'rb')
+# metrics = pickle.load(f)
+# f.close()
+
+
 # Metrics: method, repeat, test type(clean, watermarked, watermark), result type(loss, accuracy)
 
-result_indices_to_plot = np.array([0,1,2])
+result_indices_to_plot = np.array([0,1,3,4,5,6,7,8,9,10,11,13])
 
 watermarkacc = np.array(metrics)[:, :, 2, 1][result_indices_to_plot]
 watermarkacc = np.transpose(watermarkacc)
@@ -21,26 +40,58 @@ watermarkedacc = np.transpose(watermarkedacc)
 cleanacc = np.array(metrics)[:, :, 0, 1][result_indices_to_plot]
 cleanacc = np.transpose(cleanacc)
 
-plt.figure(figsize=(5, 4))
+fig, ax = plt.subplots()
+fig.set_size_inches(20,8)
+ax.set_ylim(0,1)
+ax.set_xlim(0,13)
 
-clean = plt.violinplot(cleanacc, showmeans=True, showextrema=True)
-watermarked = plt.violinplot(watermarkedacc, showmeans=True, showextrema=True)
-watermark = plt.violinplot(watermarkacc, showmeans=True, showextrema=True)
+showmeans = True
+showextrema = False
+showmedians = True
 
-labels = np.array(['Naive',
-                   '10% counterexamples',
-                   'Matched',
-                   'Naive',
-                   '10% counterexamples',
-                   'Matched',])
+qs = calculate_quantiles(watermarkacc)
 
-plt.xticks(np.array([1, 2, 3, 4, 5, 6])[:len(result_indices_to_plot)],
-           labels[result_indices_to_plot])
-plt.xlabel("Methods")
-plt.ylabel("Accuracy")
+clean = plt.violinplot(
+    cleanacc,
+    showmeans=showmeans,
+    showextrema=showextrema,
+    showmedians=showmedians,
+    quantiles=np.array([[.25, .75]]*len(result_indices_to_plot)).T,
+    widths=.9)
+watermarked = plt.violinplot(
+    watermarkedacc,
+    showmeans=showmeans,
+    showextrema=showextrema,
+    showmedians=showmedians,
+    quantiles=np.array([[.25, .75]]*len(result_indices_to_plot)).T,
+    widths=.9)
+watermark = plt.violinplot(
+    watermarkacc,
+    showmeans=showmeans,
+    showextrema=showextrema,
+    showmedians=showmedians,
+    quantiles=np.array([[.25, .75]]*len(result_indices_to_plot)).T,
+    widths=.9)
+
+# labels = np.array(['Debias',
+#                    'Naive',
+#                    'Unbalanced',
+#                    'cleanmix',
+#                    '10% counterexamples',
+#                    'Matched',])
+#
+# plt.xticks(np.array([1, 2, 3, 4, 5, 6])[:len(result_indices_to_plot)],
+#            labels[result_indices_to_plot])
+
+plt.xticks(np.array(range(13))[:len(result_indices_to_plot)])
+
+# plt.xlabel("Methods")
+# plt.ylabel("Accuracy")
 # plt.title("Auxiliary Watermark Accuracy")
-plt.legend([clean['bodies'][0], watermarked['bodies'][0], watermark['bodies'][0]],
-           ["$x_i$", "$x_i+n_i$", "$n_i$"])
+# plt.legend([clean['bodies'][0], watermarked['bodies'][0], watermark['bodies'][0]],
+#            ["$x_i$", "$x_i+n_i$", "$n_i$"])
+
+plt.title('means medians quartiles')
 plt.show()
 
 print(f'Aux--: {np.average(watermarkacc, 0)}')
