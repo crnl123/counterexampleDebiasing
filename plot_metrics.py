8f21ffce92f3ee99ab3ace3cfbf1099f5d774ac5
crnl123 <lausundy@yahoo.com.hk>
2025-11-03 20:27:56 +0000

    X^N_i

--- plot_metrics.py
+++ plot_metrics.py
@@ -111,10 +111,10 @@ plt.rcParams['text.usetex'] = True
 
 ax.tick_params(labelsize=fontsize)
 
-labels = np.array(['Ours:\n$X_i+N_i$\n$(X_i+N_i) \oplus C_{t_{1 \dots {|I_{-i}|}}}$\n$C_i$',
-                   'Naive:\n$X_i+N_i$',
-                   'Imbalanced:\n$X_i+N_i$\n$(X_i+N_i) \oplus C_j$\n(unbalanced)\n$C_i$',
-                   '$X_i+N_i$\n$C_i$',
+labels = np.array(['Ours:\n$X^N_i$\n$X_i^{N_i} \oplus C_{t_1},\dots$\n$C_i$',
+                   'Naive:\n$X^N_i$',
+                   'Imbalanced:\n$X^N_i$\n$X^N_i \oplus C_j$\n(unbalanced)\n$C_i$',
+                   '$X^N_i$ and $C_i$',
                    'Naive\n75%\nwatermark',
                    '10%\nexamples\n75%\nwatermark',
                    'Ours\n75%\nwatermark',
@@ -143,7 +143,7 @@ plt.ylabel("Accuracy", fontsize=fontsize)
 plt.legend([clean['bodies'][0],
             watermarked['bodies'][0],
             watermark['bodies'][0]],
-            ["$X_i$", "$X_i+N_i$", "$N_i$"],
+            ["$X_i$", "$X^N_i$", "$N_i$"],
             fontsize=fontsize,
             loc=(.88,.7)
            )
